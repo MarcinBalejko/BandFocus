a6847db32cf46d5ec3e66fc93a97fde796085b79
MarcinBalejko <mbbm9@o2.pl>
2020-10-01 22:25:38 +0200

    :white_check_mark::zap::card_file_box: Connected with MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+require("dotenv/config");
 
 const app = express();
 
+dotenv.config();
+
+// Connect Database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server up and running on port ${PORT}`)
+);
