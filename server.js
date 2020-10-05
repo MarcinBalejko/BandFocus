b3632888ec0f978f66a7237ae23fb80172fcb962
MarcinBalejko <mbbm9@o2.pl>
2020-10-05 18:44:03 +0200

    :white_check_mark: Create profile route

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-
-require("dotenv/config");
+const path = require("path");
 
 const app = express();
 
@@ -11,16 +10,24 @@ dotenv.config();
 // Connect Database
 connectDB();
 
-// Init middleware
-app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => res.send("API Running"));
+// Init Middleware
+app.use(express.json());
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
