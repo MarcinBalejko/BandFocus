e50a0725b467666d16737831a85aae0059a32b47
MarcinBalejko <mbbm9@o2.pl>
2020-11-15 01:05:40 +0100

    :white_check_mark: Imported profile actions into the navbar and the main page

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+  return <Fragment></Fragment>;
+};
+
+Profiles.propTypes = {
+  profile: PropTypes.object.isRequired,
+  getProfiles: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
