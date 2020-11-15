e0cca05c17638b5d88e8a15c11485fad1e11ae67
MarcinBalejko <mbbm9@o2.pl>
2020-11-15 23:20:31 +0100

    :white_cehck_mark::pencil2::fire: Profile component created, Minor bugs and typos removed

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfileById } from "../../actions/profile";
+
+const Profile = ({ match, getProfileById }) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  });
+  return <Fragment>profile</Fragment>;
+};
+
+Profile.propTypes = {
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  getProfileById: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
