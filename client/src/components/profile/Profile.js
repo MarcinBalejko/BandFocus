4dda60e39b005a9d3107b91b2b6ab518ba33bd78
MarcinBalejko <mbbm9@o2.pl>
2020-11-15 23:47:39 +0100

    :white_check_mark: Displaying user's profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,14 +1,40 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
 
-const Profile = ({ match, getProfileById }) => {
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  });
-  return <Fragment>profile</Fragment>;
+  }, [getProfileById]);
+
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profile.propTypes = {
