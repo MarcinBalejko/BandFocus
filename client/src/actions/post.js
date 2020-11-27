82ee808addcf89717a5904833b9fdd2ed8f72c88
MarcinBalejko <mbbm9@o2.pl>
2020-11-27 23:58:04 +0100

    :white_check_mark: Created delete post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { setAlert } from "./alert";
 import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
@@ -44,6 +45,25 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
